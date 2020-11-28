d16a4fbb2fee945cf8862427dc067b1a37c6a88e
showta0511 <mishow0511@gmail.com>
2020-11-28 12:37:58 +0900

    lesson#1

---
+++ study1.c
@@ -0,0 +1,15 @@
+#include <stdio.h> /*標準入出力の入ったファイル*/
+int main(void){
+  printf("hello,world\n");
+return 0;
+}
+
+/*
+cはコンパイラ言語
+実行ファイルに変換してから実行する
+機械語に変換して実行
+gcc -o study study1.c
+study.cを機械語に変換して
+studyというファイルにアウトプットする　
+(アウトプットするファイル名は任意)
+*/
